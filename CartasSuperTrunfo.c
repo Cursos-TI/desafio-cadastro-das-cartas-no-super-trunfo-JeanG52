2dfc0a2b4d4a6ae98b9e031de83736dae2ec33ab
Jean Carlos <jeancarlos_dss@hotmail.com>
2024-11-13 12:08:37 +0000

    SuperTrunfo Intermediario - Concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,49 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
+int main(){
+    char estado[50];
+    char codigocarta[10];
+    char nomecidade[100];
+    float populacao;
+    float areakm2;
+    float pib;
+    int pontosturisticos;
+    float densidade_populacional;
+    float pib_per_capita;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Digite a sigla do seu estado: \n");
+    scanf("%s", estado);
+
+    printf("Digite o código de sua carta: \n");
+    scanf("%s", codigocarta);
+
+    printf("Digite o nome da sua cidade: \n");
+    scanf("%s", nomecidade);
+
+    printf("Digite a quantidade de habitantes em sua cidade: \n");
+    scanf("%f", &populacao);
+
+    printf("Digite a área em km² de sua cidade: \n");
+    scanf("%f", &areakm2);
+
+    printf("Digite o PIB de sua cidade: \n");
+    scanf("%f", &pib);
+
+    printf("Digite o número de pontos turísticos de sua cidade: \n");
+    scanf("%d", &pontosturisticos);
+
+    densidade_populacional = populacao / areakm2;
+    pib_per_capita = pib / populacao;
+
+    printf("\nEstado: %s\n", estado);
+    printf("Código da carta: %s\n", codigocarta);
+    printf("Nome da cidade: %s\n", nomecidade);
+    printf("População: %.2f\n", populacao);
+    printf("Área km²: %.2f\n", areakm2);
+    printf("PIB: %.2f\n", pib);
+    printf("Pontos turísticos: %d\n", pontosturisticos);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade_populacional);
+    printf("PIB per capita: %.2f\n", pib_per_capita);
 
     return 0;
 }
